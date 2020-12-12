5f0d63413ba81ee477cfb43ea483ef7a445e6100
dudenamedjune <dudenamedjune@gmail.com>
2020-12-12 15:04:50 -0600

    add a custom page control

--- Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -4,7 +4,6 @@
 //
 //  Created by June Lara on 12/12/20.
 //
-
 import SwiftUI
 
 struct CategoryHome: View {
@@ -14,12 +13,9 @@ struct CategoryHome: View {
     var body: some View {
         NavigationView {
             List {
-                modelData.features[0].image
-                      .resizable()
-                      .scaledToFill()
-                      .frame(height: 200)
-                      .clipped()
-                      .listRowInsets(EdgeInsets())
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                    .aspectRatio(3 / 2, contentMode: .fit)
+                    .listRowInsets(EdgeInsets())
 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
@@ -46,6 +42,5 @@ struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
             .environmentObject(ModelData())
-            .previewDevice("iPhone 12")
     }
 }
