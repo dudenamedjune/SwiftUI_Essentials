e9f4861e0ae7348de37c853dba6b963850da101f
dudenamedjune <dudenamedjune@gmail.com>
2020-12-12 14:54:13 -0600

    tracking page in swiftUI view state

--- Landmarks/Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -4,21 +4,23 @@
 //
 //  Created by June Lara on 12/12/20.
 //
-
 import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
 
     var body: some View {
-        PageViewController(pages: pages)
+        VStack {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            Text("Current Page: \(currentPage)")
+        }
     }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
         PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
-            .previewDevice("iPhone 12")
             .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
